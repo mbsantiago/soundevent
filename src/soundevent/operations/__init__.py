4a271a46965b877669eaa7d177136ffbe0cc2387
mbsantiago <santiago.mbal@gmail.com>
2025-04-25 18:07:03 +0100

    Added new functions to operation __init__

--- src/soundevent/operations/__init__.py
+++ src/soundevent/operations/__init__.py
@@ -1,5 +1,15 @@
+from soundevent.operations.annotation_projects import (
+    clean_project_paths,
+    clean_project_tags,
+)
 from soundevent.operations.clip import segment_clip
+from soundevent.operations.paths import PathTransform
+from soundevent.operations.tags import TagTransform
 
 __all__ = [
+    "PathTransform",
+    "TagTransform",
+    "clean_project_paths",
+    "clean_project_tags",
     "segment_clip",
 ]
